3c7b1459e35ed89594251f9269e113701c2b22b0
TravelerBiu <1434176261@qq.com>
2019-05-07 18:35:02 +0800

    第一次提交

---
+++ student/src/com/nnxy/model/studentModel.java
@@ -0,0 +1,18 @@
+package com.nnxy.model;
+
+public class studentModel {
+private int age;
+private int sex;
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public int getSex() {
+	return sex;
+}
+public void setSex(int sex) {
+	this.sex = sex;
+}
+}
